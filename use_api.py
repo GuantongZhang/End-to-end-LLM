f09b79fed68a3b1c78fc65c5a1cb9ec35c615e4a
AadityaN <90440746+AadityaGithubFirst@users.noreply.github.com>
2025-03-12 15:18:25 -0400

    Changes done to use_api.py
    Added some changes to use_api to create annotation files with Questions and Answers.

--- use_api.py
+++ use_api.py
@@ -5,17 +5,17 @@ import os
 import openai
 
 
-# get all txt files
-def find_txt_files(directory):
-    txt_files = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".txt"):
-                txt_files.append(os.path.join(root, file))
-    return txt_files
 
-txt_files = find_txt_files("web_text")
+def find_txt_files():
+    files = []
+    for file in os.listdir(os.getcwd()+"/web_text"):
+        if file.endswith(".txt") and len(file)>4:
+            print(os.path.join(os.getcwd()+"/web_text", file))
+            files.append(os.path.join(os.getcwd()+"/web_text", file))
+    return files
 
+txt_files = find_txt_files()
+print(txt_files)
 
 load_dotenv("local.env")  # put the key in the .env file
 
@@ -26,9 +26,11 @@ client = openai.OpenAI(
 
 
 #path = 'web_text/wiki/Pittsburgh.txt'
-for txt_file in txt_files[:3]:
-    with open(txt_file, "r") as file:
+for txt_file in txt_files:
+    # Open the file with UTF-8 encoding
+    with open(txt_file, 'r', encoding='utf-8') as file:
         text = file.read()
+
 
     response = client.chat.completions.create(
         model="gpt-4o-mini", #Allowed:['gpt-4o-mini', 'gpt-4o', 'text-embedding-3-small', 'text-embedding-3-large']"
@@ -39,5 +41,6 @@ for txt_file in txt_files[:3]:
     )
 
     filename = f"annotations/{txt_file.strip('/.txt').split('/')[-1]}_annotation.txt"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(response.choices[0].message.content)
